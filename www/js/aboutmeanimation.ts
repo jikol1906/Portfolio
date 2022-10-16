4cd0dfea9acb0f840413b490f4420cf75cda0ef6
Boris Grunwald <borisgrunwald@gmail.com>
2022-10-16 17:32:59 +0200

    Removed resume

--- www/js/aboutmeanimation.ts
+++ www/js/aboutmeanimation.ts
@@ -28,22 +28,5 @@ introductionTimeline
         stagger: .1,
     }, "<25%")
 
-    const downloadResumeTimeline = gsap.timeline({
-        scrollTrigger: {
-            trigger: ".introduction__download-resume",
-            start: "50% 90%",
-        },
-        defaults: {
-            duration:1,
-            ease: "elastic.out(1, 1)"
-        }
-    })
 
-downloadResumeTimeline
-    .to(".introduction__content", {
-        "--linebreak-fade-in":100
-    })
-    .from(".introduction__download-resume", {
-        opacity: 0,
-        y: "100%",
-    },"<25%")
+
