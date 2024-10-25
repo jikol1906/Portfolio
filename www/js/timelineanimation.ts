f9b4909ebfc9768178124b1af6c2b73095098bec
Boris Grunwald <borisgrunwald@gmail.com>
2024-10-25 13:57:55 +0200

    wip

--- www/js/timelineanimation.ts
+++ www/js/timelineanimation.ts
@@ -2,7 +2,7 @@ import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 
-let lines;
+let theLine;
 let circles;
 
 const events = document.querySelectorAll('.timeline__event') as NodeListOf<HTMLDivElement>;
@@ -10,63 +10,96 @@ const events = document.querySelectorAll('.timeline__event') as NodeListOf<HTMLD
 const line = document.querySelector('.timeline__line') as HTMLDivElement;
 
 
-    lines = line.querySelectorAll('[id^=tline]') as NodeListOf<SVGGeometryElement>
+    theLine = line.querySelector('#tline') as SVGGeometryElement
     circles = line.querySelectorAll('[id^=tcirc]') as NodeListOf<SVGGeometryElement>
-    
 
-
-lines.forEach((line, i) => {
-    gsap.set(line, {
-        strokeDashoffset: Math.floor(line.getTotalLength()) + 1 ,
-        strokeDasharray: Math.floor(line.getTotalLength()) + 1 ,
+    gsap.set(theLine, {
+        strokeDashoffset: Math.floor(theLine.getTotalLength()) + 1,
+        strokeDasharray: Math.floor(theLine.getTotalLength()) + 1
     })
 
-
-
-    const tlLine = gsap.timeline({
+    gsap.to(theLine, {
         scrollTrigger: {
-            trigger: line,
-            start: "top center",
-            end:"90% center",
-            scrub:.1,
-            
+            trigger: theLine,
+            start: "top 70%",
+            end: "bottom 70%",
+            scrub: .1,
         },
-        defaults: {
-            ease: "ease-in-out"
-        }
-    })
-
-
-    tlLine
-    .to(line, {
+        ease: 'linear',
         strokeDashoffset: "0",
         duration: .4
     })
 
-    .from(circles[i], {
-        scale: 0,
-        opacity:0,
-        duration: .1,
-        transformOrigin: "center"
+    circles.forEach((circle) => {
+
+        gsap.from(circle, {
+            scrollTrigger: {
+                trigger: circle,
+                start: "-500% 50%",
+                end: "-400% 50%",
+                scrub: .5,
+            },
+            opacity:0,
+            scale: .4,
+            duration: .2,
+            transformOrigin: 'center',
+        })
     })
+    
+
+
+// lines.forEach((line, i : number) => {
+//     gsap.set(line, {
+//         strokeDashoffset: Math.floor(line.getTotalLength()) + 1 ,
+//         strokeDasharray: Math.floor(line.getTotalLength()) + 1 ,
+//     })
 
 
 
-})
+//     const tlLine = gsap.timeline({
+//         scrollTrigger: {
+//             trigger: line,
+//             start: "top center",
+//             markers: true,
+//             end:"90% center",
+//             scrub:.5,
+//         },
+//         defaults: {
+//             ease: "ease-in-out"
+//         }
+//     })
 
-events.forEach((e,i) => {
-    gsap.from(e,{
-        scrollTrigger: {
-            trigger: e,
-            start: "20% 60%",
-            toggleActions: "play none none reverse",
-            // markers:true
+
+//     tlLine
+//     .to(line, {
+//         strokeDashoffset: "0",
+//         duration: .4
+//     })
+
+//     .from(circles[i], {
+//         opacity:0,
+//         x: i % 2 === 0 ? '-20px' : '20px',
+//         duration: .4,
+//         transformOrigin: "center"
+//     })
+
+
+
+// })
+
+// events.forEach((e,i) => {
+//     gsap.from(e,{
+//         scrollTrigger: {
+//             trigger: e,
+//             start: "20% 60%",
+//             toggleActions: "play none none reverse",
+//             // markers:true
             
 
-        },
+//         },
         
-        x:i % 2 === 0 ? "10%" : "-10%",
-        opacity: 0,
-        duration:0.4
-    })
-})
+//         x:i % 2 === 0 ? "10%" : "-10%",
+//         opacity: 0,
+//         duration:0.4
+//     })
+// })
